6b20b5bfc11da9242fa23b24dfec953bbd1476b1
Alvaro <panabuntu@gmail.com>
2021-03-23 18:00:16 +0100

    add jetpack navigation

---
+++ app/src/main/java/com/buntupana/tv_application/presentation/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.buntupana.tv_application.presentation.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.buntupana.tv_application.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/com/buntupana/tv_application/presentation/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.buntupana.tv_application.presentation.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
